f992fea01e698ea15905727b35036bc06924566a
Abraham <ipseabraham@gmail.com>
2017-09-23 23:03:32 -0600

    project 1

--- Proyecto1/code/gradtarget.m
+++ Proyecto1/code/gradtarget.m
@@ -9,6 +9,8 @@ function [gW1,gW2]=gradtarget(W1,W2,X,Y)
   # X:  training set holding on the rows the input data, plus a final column 
   #     equal to 1
   # Y:  labels of the training set
+  
+  g=(1./(1+e.^(W1'*[1 1;X])));
+  gW1 = -(Y-target(W1,W2,X,Y))*target(W1,W2,X,Y)*(1-target(W1,W2,X,Y))*([1 1;g]);
     
-  # PONGA SU CODIGO AQUÍ
 endfunction;
