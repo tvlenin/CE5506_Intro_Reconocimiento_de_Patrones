b387a737aa89e43dfa1ca839becd35035a767d04
Abraham <ipseabraham@gmail.com>
2017-09-23 23:05:25 -0600

    project 1

--- Proyecto1/code/gradtarget.m
+++ Proyecto1/code/gradtarget.m
@@ -10,7 +10,7 @@ function [gW1,gW2]=gradtarget(W1,W2,X,Y)
   #     equal to 1
   # Y:  labels of the training set
   
-  g=(1./(1+e.^(W1'*[1 1;X])));
-  gW1 = -(Y-target(W1,W2,X,Y))*target(W1,W2,X,Y)*(1-target(W1,W2,X,Y))*([1 1;g]);
+  g=(1./(1+e.^(W1'*[1 1; X])));
+  gW1 = -(Y-target(W1,W2,X,Y))*target(W1,W2,X,Y)*(1-target(W1,W2,X,Y))*(g);
     
 endfunction;
