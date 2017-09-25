19ead4280d570c402be6d94b709880f92170a518
Abraham <ipseabraham@gmail.com>
2017-09-25 12:29:06 -0600

    propouse for new grad W2

--- Proyecto1/code/gradtarget.m
+++ Proyecto1/code/gradtarget.m
@@ -9,13 +9,25 @@ function [gW1, gW2]=gradtarget(W1,W2,X,Y)
   # X:  training set holding on the rows the input data, plus a final column 
   #     equal to 1
   # Y:  labels of the training set
+  g1=ones(1,1);
   
   D = sum((predict(W1,W2,X)-Y),1);
-  g=(1./(1+e.^(-W1*[ones(rows(X),1) X]')));
+  pre=repmat(D,rows(X),1);
+  H1=(1./(1+e.^(-W1*[ones(rows(X),1) X]')))
+  r=1-predict(W1,W2,X);
+  g2=-(pre.*r.*(1-r))'*(H1)'
   
-  g1=g'*(1-g)*([ones(rows(X),1) X]);
-  gW1 = -((((repmat(D,rows(X),1))'*predict(W1,W2,X))*(1-predict(W1,W2,X))')'*W2)'*(g1);
+  
+  
+  
+  
+  
+  #D = sum((predict(W1,W2,X)-Y),1);
+  #g=(1./(1+e.^(-W1*[ones(rows(X),1) X]')));
+  
+  #g1=g'*(1-g)*([ones(rows(X),1) X]);
+  #gW1 = -((((repmat(D,rows(X),1))'*predict(W1,W2,X))*(1-predict(W1,W2,X))')'*W2)'*(g1);
 
-  gW2 = -((((repmat(D,rows(X),1))'*predict(W1,W2,X))*(1-predict(W1,W2,X))')*[ones(1,rows(X)); g]')';
+  #gW2 = -((((repmat(D,rows(X),1))'*predict(W1,W2,X))*(1-predict(W1,W2,X))')*[ones(1,rows(X)); g]')';
 
 endfunction;
