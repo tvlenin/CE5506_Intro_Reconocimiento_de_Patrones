5795ac5278a374b511ac3e1d3994ec4fea5b5792
Abraham <ipseabraham@gmail.com>
2017-10-09 14:56:17 -0600

    size

--- Proyecto1/code/data_evaluation.m
+++ Proyecto1/code/data_evaluation.m
@@ -8,8 +8,8 @@ endfunction;
 
 
 function [ret] = confusionmat(v1, v2)
-    values = union(unique(v1), unique(v2));
-    ret = zeros(rows(values), rows(values));
+    values = union(unique(v1), unique(v2))
+    ret = zeros(size(values), size(values));
     for i = 1:size(v1)
        i1 = find(values == v1(i));
        i2 = find(values == v2(i));
