f0815c765f9edf6c9847d213b1811dbfdb761f6c
Abraham <ipseabraham@gmail.com>
2017-11-15 12:30:20 -0600

    ok

--- Proyecto2/naive_bayes.py
+++ Proyecto2/naive_bayes.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 def predict(n_class, array):
-    ans=[0]*(n_class)
+    ans=np.zeros(n_class)
     for i in range(0,len(array),1):
-        ans[array[i]]=1
+        ans[int(array[i])]=1
     return ans
