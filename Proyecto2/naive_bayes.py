8e0d9f3442c1e3a6154f1bda24c84d1e0b31847f
Abraham <ipseabraham@gmail.com>
2017-11-15 12:15:37 -0600

    solo cuenta unos

--- Proyecto2/naive_bayes.py
+++ Proyecto2/naive_bayes.py
@@ -1,7 +1,6 @@
 def predict(n_class, array):
     ans=[0]*(n_class)
     for i in range(0,len(array)-1,1):
-        ans[array[i]]=ans[array[i]]+1
-    print(ans)
+        ans[array[i]]=+1
+    return ans
         
-predict(6,[1,2,3,4,5,0,2,1,3,6])
